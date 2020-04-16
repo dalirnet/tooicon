adabe35272bb3d672a94d8162a29967c3b7e84ad
AmirRezaDalir <dalirnet@gmail.com>
2020-04-16 17:00:29 +0430

    svg font

--- src/map.js
+++ src/map.js
@@ -1,13 +1,56 @@
 const _ = require('lodash')
+const fs = require('fs')
+const path = require('path')
+const svgConvertStream = require('svgicons2svgfont')
 const list = require('./list.json')
+const fontName = 'tooIcon'
+const unicodeStart = 57500
 
 let icons = {}
+let index = 0
 _.forEach(list, (collection) => {
-    _.forEach(collection, (svg, icon) => {
+    _.forEach(collection, (status, icon) => {
         if (_.has(icons, icon)) {
             console.log(`Duplicate item [${icon}]`)
+        } else if (status === true) {
+            icons[icon] = unicodeStart + index
+            index++
         }
-        icons[icon] = ''
     })
 })
-console.log(_.size(icons))
+
+const fontStream = new svgConvertStream({
+    fontName,
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fixedWidth: true,
+    fontHeight: 2400,
+    centerHorizontally: true,
+    normalize: true,
+    round: 1,
+    metadata: fontName,
+})
+
+fontStream
+    .pipe(
+        fs.createWriteStream(
+            path.join(__dirname, `../dist/font/${fontName}.svg`)
+        )
+    )
+    .on('finish', function () {})
+    .on('error', function (err) {
+        console.log(err)
+    })
+
+_.forEach(icons, (code, name) => {
+    const glyph = fs.createReadStream(
+        path.join(__dirname, `../svg/fill/${name}.svg`)
+    )
+    glyph.metadata = {
+        unicode: [String.fromCharCode(code)],
+        name,
+    }
+    fontStream.write(glyph)
+})
+
+fontStream.end()
